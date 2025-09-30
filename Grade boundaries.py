7c3bc0bdb962a791d8cc989761eec952889abef8
lukas445 <ld05267634@priestley.ac.uk>
2025-09-30 14:26:57 +0100

    Add files via upload

---
+++ Grade boundaries.py
@@ -0,0 +1,17 @@
+mark=int(input('Enter your mark for Computer Science Paper 2:'))
+if(mark>=48):
+    print('Your grade is an A')
+elif(mark>=41):
+    print('Your grade is a B')
+elif(mark>=34):
+    print('Your grade is a C')
+elif(mark>=28):
+    print('Your grade is a D')
+elif(mark>=22):
+    print('Your grade is an E')
+elif(mark>=0):
+    print('Your grade is a U')
+elif(mark>=71):
+    print('The maximum amount of marks is 70')
+elif(mark<=-1):
+    print('The minimum amount of marks is 0')
