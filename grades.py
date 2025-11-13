67f202bd01bf50ea428ff03a3b94d7bb47f30d6d
lukas445 <ld05267634@priestley.ac.uk>
2025-11-13 11:08:09 +0000

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,16 @@
+grade = int(input('Please enter your marks: '))
+
+if grade >=71:
+    print('The test is out of 70.')
+elif grade >=46:
+    print('You achieved an A!')
+elif grade >=40:
+    print('You achieved a B!')
+elif grade >=34:
+    print('You achieved a C!')
+elif grade >=28:
+    print('You achieved a D!')
+elif grade >=22:
+    print('You achieved a E!')
+elif grade < 22:
+    print('You achieved a U!')
