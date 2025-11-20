c926b6e58465d1932907b23d45273681cf1abaeb
lukas445 <ld05267634@priestley.ac.uk>
2025-11-20 13:54:09 +0000

    Add files via upload

--- bubble sort advanced version.py
+++ bubble sort advanced version.py
@@ -3,7 +3,7 @@ items = [12, 54, 5, 7, 32, 34]
 n = len(items) - 1
 swapped = True
 
-while (swapped):
+while swapped and n > 0:
     swapped = False
     for index in range(0, n):
         if (items[index] > items[index + 1]):
@@ -12,5 +12,7 @@ while (swapped):
             items[index + 1] = temp
 
             swapped = True
+
+    n = n - 1
 
 print(items)
